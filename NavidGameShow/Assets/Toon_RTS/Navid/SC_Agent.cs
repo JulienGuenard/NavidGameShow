507313bc2ed9c31d6fbb5c28ae38eabdc9aaade6
Navid <navid.soroosh.markazi@gmail.com>
2018-03-03 19:42:39 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ NavidGameShow/Assets/Toon_RTS/Navid/SC_Agent.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class SC_Agent : MonoBehaviour {
+
+    public Transform _target;
+    private NavMeshAgent _navMeshAgent;
+    private Animator _animator;
+
+
+    void Start () {
+		_navMeshAgent = this.transform.GetComponent<NavMeshAgent>();
+        _animator = this.transform.GetComponent<Animator>();
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                _navMeshAgent.destination = hit.point;
+                _animator.SetBool("Walk", true);
+            }
+        }
+    }
+}
