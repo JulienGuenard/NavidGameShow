ae4753b5e643606ff41d0fcf3d3a5760549e17ed
JulienGuenard <julienguenard01@gmail.com>
2018-03-03 19:12:14 +0100

    bouger avec click droit

--- NavidGameShow/Assets/ControllerBuild.cs
+++ NavidGameShow/Assets/ControllerBuild.cs
@@ -7,16 +7,32 @@ public class ControllerBuild : MonoBehaviour {
 
 public static ControllerBuild Instance;
 
+GameObject rootCamera;
+
     void Awake ()
     {
     Instance = this;
+
+      rootCamera = GameObject.Find("rootCamera");
     }
 
 	void Update () 
     {
+      Debug.Log(Input.mousePosition);
 
-    RaycastHit hit;
-		if (Input.GetKeyDown(KeyCode.Mouse0))
+      if (Input.GetKeyDown(KeyCode.Mouse0))
+      BuildAtCurseur();
+
+      if (Input.GetKey(KeyCode.Mouse1))
+      MoveCamera();
+
+
+	}
+
+    void BuildAtCurseur ()
+    {
+      RaycastHit hit;
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
           Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit))
@@ -24,5 +40,15 @@ public static ControllerBuild Instance;
               PullingManager.Instance.Build(hit.point + new Vector3 (0,transform.localScale.y/2,0), typeBuilding.Barrack);
             }  
         }
-	}
+    }
+
+    void MoveCamera ()
+    {
+      rootCamera.transform.position += new Vector3(Input.GetAxis("Mouse Y") + Input.GetAxis("Mouse X"), 0, Input.GetAxis("Mouse Y") - Input.GetAxis("Mouse X"));
+    }
+
+  public void MoveCameraAtBound ()
+    {
+      rootCamera.transform.position += new Vector3(Input.GetAxis("Mouse Y") + Input.GetAxis("Mouse X"), 0, Input.GetAxis("Mouse Y") - Input.GetAxis("Mouse X"));
+    }
 }
